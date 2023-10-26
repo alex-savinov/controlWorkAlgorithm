d266849dd68aa469c7f3efa4eb9ce34abff8ef49
Alex <sp.savinov@gmail.com>
2023-10-26 17:51:32 +0300

    End task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tree tree = new Tree();
+        tree.add(7);
+        tree.add(2);
+        tree.add(4);
+        tree.add(1);
+        tree.add(6);
+        tree.add(12);
+
+        int test = 0;
     }
 }
